0c7979ee6d43da9a22edc8f34f7b6bfbed2c4742
Ronit Shternfeld <ronits@earnix.com>
2023-07-22 22:41:02 +0300

    styled components

--- src/view/TopBar.tsx
+++ src/view/TopBar.tsx
@@ -1,18 +1,44 @@
 import { ReactNode } from "react";
 // import reactLogo from '/icons/logo.svg';// load at runtime from public
 // import logo from '../assets/react.svg';// embedded in the bundle at build
+import styled, {keyframes} from 'styled-components';
 
 interface Props {
   children?: ReactNode;
 }
 
+const spin = keyframes`
+to {
+  transform: rotate(360deg);
+}
+`
+
+const Div = styled.div`
+background: slategray;
+  width: 100%;
+  height: 70px;
+  padding: 20px;
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`
+
+const Img = styled.img`
+height: 5rem;
+  animation: ${spin} 2s linear infinite;
+`
+
 export function TopBar({ children }: Props) {
   return (
-    <div className="top-bar">
+    <Div>
       {children}
-      <img className="logo" src="./icons/logo.svg" alt="logo" />
+      <Img src="./icons/logo.svg" alt="logo" />
       {/* <img className="logo" src={reactLogo} alt="logo" /> */}
       {/* <img className="logo" src={logo} alt="logo" /> */}
-    </div>
+    </Div>
   );
 }
